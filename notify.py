c8d661f8f580529a21e288e5a243663e7fee1dfd
kpzhao <36727707+kpzhao@users.noreply.github.com>
2021-10-23 00:25:32 +0800

    Create notify.py

---
+++ notify.py
@@ -0,0 +1,10 @@
+def sendTg:
+    try:
+        content = test
+        url = f'https://api.telegram.org/bot2053091895:AAE8t09sW--zV_muyfXpf8Euv_6EunzyMLE/sendMessage?chat_id=549864937&text={content}'     
+        session = requests.Session()
+        resp = session.post(url)
+        print(resp)
+    except Exception as e:
+        print('Tg通知推送异常，原因为: ' + str(e))
+        print(traceback.format_exc())
