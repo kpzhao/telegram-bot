090de03e41dc4a65b4553ba2a2e48d315fc764de
kpzhao <36727707+kpzhao@users.noreply.github.com>
2021-10-23 00:49:09 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,7 +1,6 @@
 def sendTg():
     try:
-        content = test
-        url = f'https://api.telegram.org/bot2053091895:AAE8t09sW--zV_muyfXpf8Euv_6EunzyMLE/sendMessage?chat_id=549864937&text={content}'     
+        url = f'https://api.telegram.org/bot2053091895:AAE8t09sW--zV_muyfXpf8Euv_6EunzyMLE/sendMessage?chat_id=549864937&text=test'     
         session = requests.Session()
         resp = session.post(url)
         print(resp)
